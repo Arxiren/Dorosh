3d8dfd05e37408993fbd9e5d3d892aaa4d675179
Arxiren <arxiren@mail.ru>
2016-12-27 21:42:37 +0200

    Update TitlesFrame.java
    Improved code

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -1,13 +1,14 @@
 /*
  * Decompiled with CFR 0_118.
  */
-import java.awt.Component;
 import javax.swing.JFrame;
+/*Unused import 'import java.awt.Component;*/
 import javax.swing.SwingUtilities;
 
-public class TitlesFrame
+class TitlesFrame /*Can be package-private*/
 extends JFrame {
-    public TitlesFrame() {
+    private TitlesFrame() /*Can be private*/
+    {
         this.initUI();
     }
 
@@ -15,20 +16,17 @@ extends JFrame {
     {
         this.setTitle("\u041a\u0440\u0438\u0432\u044b\u0435 \u0444\u0438\u0433\u0443\u0440\u044b");
         this.setDefaultCloseOperation(3);
-        this.add(new TitlesPanel(78));
+        this.add(new TitlesPanel());/*Actual value of parameter ''_shape'' is always ''78''*/
         this.setSize(350, 350);
         this.setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-
-            @Override
-            public void run() /*переопределения метода*/
-            {
-                TitlesFrame ps = new TitlesFrame();
-                ps.setVisible(true);
-            }
+        /*переопределения метода*/
+        SwingUtilities.invokeLater(() -> /*Anonymous new Runnable() can be replaced with lambda*/
+        {
+            TitlesFrame ps = new TitlesFrame();
+            ps.setVisible(true);
         });
     }
 
